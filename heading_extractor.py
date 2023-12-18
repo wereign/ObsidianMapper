c1cfbd929806c467adb440a3f4c58225b0b916e3
virenn <virenn9999@gmail.com>
2023-12-18 15:16:52 +0530

    Tree Creation Refactored

--- heading_extractor.py
+++ heading_extractor.py
@@ -1,27 +1,10 @@
+import uuid
+import os
 
 file_path = './media/4.Data Engineering.md'
 
-def get_file(file_path:str):
-    with open(file_path) as md_file:
-        all_lines = md_file.readlines()
-    return all_lines
 
-def classify_header(line):   
-    h1_id = '# '
-    h2_id = '## '
-    h3_id = '### '
 
-    if line[:len(h1_id)] == h1_id:
-        return 1
-
-    elif line[:len(h2_id)] == h2_id:
-        return 2
-
-    elif line[:len(h3_id)] == h3_id:
-        return 3
-
-    else:
-        return -1 # using 0 as file node level.
 
 class Node:
     def __init__(self,string,level):
@@ -48,45 +31,77 @@ class Node:
 
         return self.__repr__()
 
-def create_nodes_list(all_lines,file_name):
 
-    root_node_content = file_name[:-2]
-    root_node = Node(root_node_content,level=0)
-    nodes = [root_node]
-    for line in all_lines:
-
-        level = classify_header(line)
-
-        if level > 0:
-
-            node = Node(line,level)
-            nodes.append(node)
-    
-    return nodes
-
-all_lines = get_file(file_path)
-
-all_nodes = create_nodes_list(all_lines,file_path) # file name and path will be different eventually
-
-def construct_tree(node_list):
-    
-    for idx in range(len(node_list)-1,0,-1):
+class TreeFunctions:
+    def __init__(self,file_path):
+        self.file_name = os.path.basename(file_path)
+        self.file_path = self.file_path
         
-        # taking node, find the parent and the append the node to the parent.
-        node = node_list[idx]
-        for candidate_idx in range(idx,-1,-1):
-            candidate_node = node_list[candidate_idx]
+    
+    def get_file(self):
+        file_path = self.file_path
+        with open(file_path) as md_file:
+            all_lines = md_file.readlines()
+        return all_lines
+    
+    def construct_tree(self):
+
+        all_lines = self.get_file()
+        all_nodes = self.create_nodes_list(all_lines,self.file_name)
+        tree_node = self.construct_tree(all_nodes)
+
+        return tree_node
+    
+    @staticmethod
+    def classify_header(line):   
+        h1_id = '# '
+        h2_id = '## '
+        h3_id = '### '
+
+        if line[:len(h1_id)] == h1_id:
+            return 1
+
+        elif line[:len(h2_id)] == h2_id:
+            return 2
+
+        elif line[:len(h3_id)] == h3_id:
+            return 3
+
+        else:
+            return -1 # using 0 as file node level.
+
+    @staticmethod
+    def create_nodes_list(all_lines,file_name):
+
+        root_node_content = file_name[:-2]
+        root_node = Node(root_node_content,level=0)
+        nodes = [root_node]
+        for line in all_lines:
+
+            level = classify_header(line)
+
+            if level > 0:
+
+                node = Node(line,level)
+                nodes.append(node)
+        
+        return nodes
+
+    @staticmethod
+    def construct_tree(node_list):
+    
+        for idx in range(len(node_list)-1,0,-1):
             
-            if candidate_node.level == node.level - 1:
-                candidate_node.add_children(node)
-                break
+            # taking node, find the parent and the append the node to the parent.
+            node = node_list[idx]
+            for candidate_idx in range(idx,-1,-1):
+                candidate_node = node_list[candidate_idx]
+                
+                if candidate_node.level == node.level - 1:
+                    candidate_node.add_children(node)
+                    break
+        
     
-    
-    return node_list[0]
-
-root_node = construct_tree(all_nodes)
-
-print(root_node)
-print(root_node.children)
+        return node_list[0]
 
     
